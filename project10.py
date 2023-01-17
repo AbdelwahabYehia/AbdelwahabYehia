b7452a4e4a8e6f6cdc3835ec657e62563309d4c9
AbdelwahabYehia <114296696+AbdelwahabYehia@users.noreply.github.com>
2023-01-17 14:20:31 +0200

    Add files via upload

---
+++ project10.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+
+
+
+# In[3]:
+
+
+f = open("D:\interview\hello.txt")
+
+lines = f.readlines()
+
+for i in lines[-2 :]:
+    print(i,end=" ")
+
+
+# In[ ]:
+
+
+
+
